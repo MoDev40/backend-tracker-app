2437e763aee7d7e8c299573db1e9710374dfaedf
M40 <modev.404@gmail.com>
2025-05-31 17:24:14 +0300

    created task model schema

---
+++ models/task.schema.js
@@ -0,0 +1,29 @@
+const { Schema, model, Types } = require("mongoose");
+
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  assignee: {
+    type: [],
+    required: true,
+  },
+  isDone: {
+    type: Boolean,
+    default: false,
+  },
+  ownedBy: {
+    type: Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const Task = model("Task", taskSchema);
+
+module.exports = Task;
