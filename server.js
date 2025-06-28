3a29aeaa912e7b7329b4ba347215e8274a8b815a
M40 <modev.404@gmail.com>
2025-06-28 19:19:03 +0300

    Update

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/user.route");
-<<<<<<< HEAD
-const taskRoutes = require("./routes/task.route");
-
-
-=======
 const projectRoutes = require("./routes/project.route");
 const taskRoutes = require("./routes/task.route");
 const cors = require("cors");
->>>>>>> 46c358a8a48fbca4e453ef957635a39998f6cc88
 
 const PORT = process.env.PORT || 8000;
 const app = express();
@@ -22,12 +16,6 @@ app.use(cors({
 
 // Routes
 app.use("/api/users", userRoutes)
-<<<<<<< HEAD
-app.use("/api/tasks",taskRoutes)
-=======
-app.use("/api/projects", projectRoutes)
-app.use("/api/tasks", taskRoutes)
->>>>>>> 46c358a8a48fbca4e453ef957635a39998f6cc88
 
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Health Check - Tracker API is running'})
